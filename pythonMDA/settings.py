397dcb8ca125cfb15fd8532bf9d26c0f5a863c68
Javier <caballerojavier13@gmail.com>
2014-10-20 10:44:01 -0300

    Correción cambios de la configuración
    Cambios para realizar el deploy en Nitrous.IO

--- pythonMDA/settings.py
+++ pythonMDA/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pythonMDA.apps.home',
@@ -46,7 +45,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
